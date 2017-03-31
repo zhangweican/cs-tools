f9a7a9ec8e55a83bd8828f06cd0bfa99806b4ad3
zhangweican <515632400@qq.com>
2017-03-31 13:50:55 +0800

    修改一个错误代码

--- trunk/src/main/java/com/leweiyou/tools/page/PageData.java
+++ trunk/src/main/java/com/leweiyou/tools/page/PageData.java
@@ -32,18 +32,17 @@ public class PageData extends HashMap implements Map{
 		Map returnMap = new HashMap(); 
 		Iterator entries = properties.entrySet().iterator(); 
 		Map.Entry entry; 
-		String name = "";  
-		String value = "";  
 		while (entries.hasNext()) {
+			String value = "";  
 			entry = (Map.Entry) entries.next(); 
-			name = (String) entry.getKey(); 
+			String name = (String) entry.getKey(); 
 			Object valueObj = entry.getValue(); 
 			if(null == valueObj){ 
 				value = ""; 
 			}else if(valueObj instanceof String[]){ 
 				String[] values = (String[])valueObj;
 				for(int i=0;i<values.length;i++){ 
-					 value = values[i] + ",";
+					 value += values[i] + ",";
 				}
 				value = value.substring(0, value.length()-1); 
 			}else{
