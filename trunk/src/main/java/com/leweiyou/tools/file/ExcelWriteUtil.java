4a31db972d91c10bba3efd61ed8d1ac2daa4ea8f
zhangweican <515632400@qq.com>
2019-01-22 15:41:30 +0800

    支持2007导出

--- trunk/src/main/java/com/leweiyou/tools/file/ExcelWriteUtil.java
+++ trunk/src/main/java/com/leweiyou/tools/file/ExcelWriteUtil.java
@@ -2,6 +2,7 @@ package com.leweiyou.tools.file;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -10,16 +11,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.leweiyou.tools.bean.BeanUtils;
 import com.leweiyou.tools.date.DateUtil;
 
 /**
- * <font color=red>写入现在只实现了写入xls文件，对2017Excel暂时不支持</font>
+ * <font color=red>导出支持2003，也支持2017</font>
  * @author Zhangweican
  *
  */
@@ -49,17 +52,21 @@ public class ExcelWriteUtil {
 
 		try {
 			// 创建XLS文档
-			HSSFWorkbook document = new HSSFWorkbook();
-
+			Workbook document = null;
+			String fileName = file.getName();
+			String extension = fileName.lastIndexOf(".") == -1 ? "" : fileName.substring(fileName.lastIndexOf(".") + 1);
+			if ("xls".equalsIgnoreCase(extension)) {
+				document = getWorkbook(false);
+			} else if ("xlsx".equalsIgnoreCase(extension)) {
+				document = getWorkbook(true);
+			} else {
+				throw new IOException("不支持的文件类型");
+			}
 			// 创建Sheet
-			HSSFSheet sheet = document.createSheet("SheetOne");
-
+			Sheet sheet = document.createSheet("SheetOne");
 			creatExcelHeader(title,headNames, sheet);
-
 			createExcelBody(voFieldNames,voList, sheet);
-
 			createExcelFile(file, document);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception("error.export.excel.file");
@@ -89,17 +96,12 @@ public class ExcelWriteUtil {
 		
 		try {
 			// 创建XLS文档
-			HSSFWorkbook document = new HSSFWorkbook();
-			
+			Workbook document = getWorkbook(true);
 			// 创建Sheet
-			HSSFSheet sheet = document.createSheet("SheetOne");
-			
+			Sheet sheet = document.createSheet("SheetOne");
 			creatExcelHeader(title,headNames, sheet);
-			
 			createExcelBody(voFieldNames,voList, sheet);
-			
 			document.write(stream);
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception("error.export.excel.file");
@@ -130,24 +132,19 @@ public class ExcelWriteUtil {
 		
 		try {
 			// 创建XLS文档
-			HSSFWorkbook document = new HSSFWorkbook();
+			Workbook document = getWorkbook(true);
 			for(Map.Entry<String,List> entry:voListMap.entrySet()){
 				// 创建Sheet
-				HSSFSheet sheet = document.createSheet(entry.getKey());
-				
+				Sheet sheet = document.createSheet(entry.getKey());
 				creatExcelHeader(title,headNames, sheet);
-				
 				createExcelBody(voFieldNames,entry.getValue(), sheet);
-				
 			}
 			document.write(stream);
-	
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception("error.export.excel.file");
 		}
 	}
-	
 	
 	/**
 	 * 创建ExcelHSSFWorkbook，用于页面输出
@@ -159,7 +156,7 @@ public class ExcelWriteUtil {
 	 * @return
 	 * @throws BaseAppException
 	 */
-	public static HSSFWorkbook exportExcel(String title, String[] headNames, String[] voFieldNames, List voList) throws Exception {
+	public static Workbook exportExcel(String title, String[] headNames, String[] voFieldNames, List voList) throws Exception {
 		// 验证数据有效性 
 		if (null == voFieldNames || voFieldNames.length < 1)
 			throw new Exception("error.export.excel.file");
@@ -170,16 +167,12 @@ public class ExcelWriteUtil {
 
 		try {
 			// 创建XLS文档
-			HSSFWorkbook document = new HSSFWorkbook();
+			Workbook document = getWorkbook(true);
 			// 创建Sheet
-			HSSFSheet sheet = document.createSheet("SheetOne");
-
+			Sheet sheet = document.createSheet("SheetOne");
 			creatExcelHeader(title,headNames, sheet);
-
 			createExcelBody(voFieldNames,voList, sheet); 
-			
 			return document;
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception("Create Excel Document is Error!");
@@ -193,19 +186,17 @@ public class ExcelWriteUtil {
 	 * @param excelHeads
 	 * @param sheet
 	 */
-	private static void creatExcelHeader(String title,String[] headNames, HSSFSheet sheet) { 
+	private static void creatExcelHeader(String title,String[] headNames, Sheet sheet) { 
 		int column = 0;
-		
 		//创建标题
-		HSSFRow titleRow = sheet.createRow(0);
-		HSSFCell cell1 = titleRow.createCell(column);
+		Row titleRow = sheet.createRow(0);
+		Cell cell1 = titleRow.createCell(column);
 		cell1.setCellValue(title);
-			
 		//创建列表名
 		titleRow = sheet.createRow(1);
 		for (int i=0;i<headNames.length;i++) {
 			String head = headNames[i];
-			HSSFCell cell = titleRow.createCell(column++);
+			Cell cell = titleRow.createCell(column++);
 			cell.setCellValue(head);
 		}
 	}
@@ -216,19 +207,14 @@ public class ExcelWriteUtil {
 	 * @param excelContents
 	 * @param sheet
 	 */
-	private static void createExcelBody(String[] voFieldNames,List voList,HSSFSheet sheet) throws Exception{
-  
+	private static void createExcelBody(String[] voFieldNames,List voList,Sheet sheet) throws Exception{
 		int row = 2, column = 0; 
-
 		for (int i = 0; i < voList.size(); i++) {
 			Object vo = voList.get(i);
-
-			HSSFRow eachRow = sheet.createRow(row ++);
+			Row eachRow = sheet.createRow(row ++);
 			for (int j = 0; j < voFieldNames.length; j++) {
-
 				Object cellContent = BeanUtils.getProperty(vo, voFieldNames[j]);
-
-				HSSFCell cell = eachRow.createCell(column++);
+				Cell cell = eachRow.createCell(column++);
 				if (cellContent == null) {
 					cell.setCellValue("");
 				} else {
@@ -241,7 +227,6 @@ public class ExcelWriteUtil {
 			}
 			column = 0;
 		}
-		 
 	} 
 
 	/**
@@ -251,14 +236,11 @@ public class ExcelWriteUtil {
 	 * @param document
 	 * @throws Exception
 	 */
-	private static void createExcelFile(File file, HSSFWorkbook document) throws Exception {
- 
+	private static void createExcelFile(File file, Workbook document) throws Exception {
 		if(!file.getParentFile().exists()){
 			file.getParentFile().mkdirs();
 		}
-		
 		FileOutputStream fos = null;
-		
 		try {
 			fos = new FileOutputStream(file);
 			document.write(fos);
@@ -274,16 +256,25 @@ public class ExcelWriteUtil {
 			}
 		}
 	}
-
-
-
 	
-
-
+	/**
+	 * 获取Workbook
+	 * @param isXlsx
+	 * @return
+	 * @throws IOException
+	 */
+	private static Workbook getWorkbook(boolean isXlsx) throws IOException{
+		if(isXlsx){
+			XSSFWorkbook hwb = new XSSFWorkbook();
+			return hwb;
+		}else{
+			HSSFWorkbook hwb = new HSSFWorkbook();
+			return hwb;
+		}
+	}
 
 
 	public static void main(String[] args) { 
-		
 		try {  
 			String title = "MueCard Fare Calculate Log List";
 			String[] fieldNames = { "value", "delflag", "describe", "nameCn", "nameEn", "type"}; 
@@ -297,12 +288,8 @@ public class ExcelWriteUtil {
 			map.put("nameCn", 101.56F);
 			map.put("nameEn", Calendar.getInstance().getTime());
 			map.put("type", null);
-			
-			
 			excelContents.add(map); 
-			 
 			ExcelWriteUtil.exportExcel(new File("E:\\document\\项目相关\\test.xlsx"),title,fieldNames, fieldNames,excelContents); 
-			
 		} catch (Exception e) { 
 			e.printStackTrace();
 		} 
